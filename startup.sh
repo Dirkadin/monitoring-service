65ec7655ab6b8764c8b986d7c9e416cfa71be4b8
Dirkadin <dirkadin@gmail.com>
2019-11-10 16:32:09 -0600

    bleh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo Building image
 docker image build -t monitoring-service .
-docker run -d -p 8080:3001 --mount type=volume,source=hotburger-logs,target=/app/logs -it project2
+docker run -d -p 8080:3001 --mount type=volume,source=hotburger-logs,target=/app/logs -it monitoring-service
